b90252c48da96d8b295092af2023b8a89d6e4eea
venky1-star <67516581+venky1-star@users.noreply.github.com>
2021-04-04 11:46:58 +0530

    Add files via upload

---
+++ pro.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int age;
+cout<<"Enter the age of person : ";
+cin>>age;
+if(age>=18)
+{
+cout<<"Major\n";
+cout<<"He/she can vote ";
+}
+else{
+	cout<<"He/She is minor";
+}
+}
